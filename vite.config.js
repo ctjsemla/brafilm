215643f5992b50dded31027056041578db43f79e
Lucifer <lucifer@Lucifer-MacBook-Air.local>
2026-05-17 06:18:20 -0700

    base path düzeltildi

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub Pages: repo adınız "brafilm" ise base: '/brafilm/'
-// Kullanıcı adı.github.io repo'su ise base: '/'
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES === 'true' ? '/brafilm/' : '/',
+  base: '/',
 })
